5015a09e0877a4fad23ef58ffb408947a6822754
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2020-10-01 16:01:16 +0300

    Add AddNoteActivity view and activity with menu item

---
+++ app/src/main/java/com/sarmaru/mihai/androidarchitecturenoteapp/AddNoteActivity.java
@@ -0,0 +1,53 @@
+package com.sarmaru.mihai.androidarchitecturenoteapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+
+public class AddNoteActivity extends AppCompatActivity {
+    private EditText editTextTitle;
+    private EditText editTextDescription;
+    private NumberPicker numberPickerPriority;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_note);
+
+        // Find views
+        editTextTitle = findViewById(R.id.edit_text_title);
+        editTextDescription = findViewById(R.id.edit_text_description);
+        numberPickerPriority = findViewById(R.id.number_picker_priority);
+
+        numberPickerPriority.setMinValue(1);
+        numberPickerPriority.setMaxValue(10);
+
+        // Set Actionbar icon and title
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close);
+        setTitle("Add Note");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.add_note_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.save_note:
+                // Save note here
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
